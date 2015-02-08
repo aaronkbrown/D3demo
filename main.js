324a1fcb6e4d59857301233707a5cf317c02068f
Aaron Brown <aaron.kaoru.brown@gmail.com>
2015-02-07 16:13:03 -0800

    simple bar chart

--- main.js
+++ main.js
@@ -3,6 +3,23 @@ var main = d3.select("main");
 var dataset = [5, 10, 15, 20, 25];
 var graphDiv = main.append("div");
 
+d3.select("#barchart")
+  .selectAll("div")
+  .data(dataset)
+  .enter()
+  .append("div")
+  .attr("class", "bar")
+  .style("height", function(d){
+    var barHeight = d * 5;
+    return barHeight + "px";
+  })
+  .style("background-color", function(d){
+    if(d > 15){
+      return "#ff0000";
+    }
+  });
+
+/**
 main.selectAll("p").data(dataset).enter().append("p").text(function(someData){
   return someData;
 }).style("color", function(d){
@@ -12,3 +29,4 @@ main.selectAll("p").data(dataset).enter().append("p").text(function(someData){
     return "#000000";
   }
 });
+*/
