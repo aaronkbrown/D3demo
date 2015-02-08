ebaae0ad8eb6eb0df26c3c66ec1f89cced4815af
Aaron Brown <aaron.kaoru.brown@gmail.com>
2015-02-07 17:51:32 -0800

    data.json, svg circles chart

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 
 var main = d3.select("main");
-var dataset = [5, 10, 15, 20, 25];
+//var dataset = [5, 10, 15, 20, 25];
 var graphDiv = main.append("div");
+var dataset = [];
 
-d3.select("#barchart")
+$.getJSON("data.json", function(d){
+  var nDataSize = d.dataPoints.length;
+  for(i = 0; i < nDataSize; i++){
+    dataset[i] = d.dataPoints[i];
+  }
+}).done(function(){
+
+  d3.select("#barchart")
   .selectAll("div")
   .data(dataset)
   .enter()
@@ -19,6 +27,33 @@ d3.select("#barchart")
     }
   });
 
+  var nHeight = 400;
+  var nWidth = 800;
+  var svg = d3.select("main").append("svg");
+  svg.attr("width", nWidth).attr("height", nHeight);
+
+  var circles = svg.selectAll("circle").data(dataset).enter().append("circle");
+
+  circles.attr("cx", function(d, i){
+    return (i * 50) + 25;
+  })
+  .attr("cy", nHeight / 2)
+  .attr("r", function(d){
+    return d;
+  })
+  .attr("fill", function(d){
+    if(d > 15){
+      return "#ff40c0";
+    } else {
+      return "#a01080";
+    }
+  })
+  .attr("stroke", "#800000")
+  .attr("stroke-width", function(d){
+    return d/2;
+  });
+
+});
 /**
 main.selectAll("p").data(dataset).enter().append("p").text(function(someData){
   return someData;
